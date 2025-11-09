24183a64863d52eb680f2473f5f03c47cb8eb70c
SilverBlure <oesterle.ni@onlinehome.de>
2025-11-09 17:52:21 +0100

    add firebase

--- src/app/firebase-services/note-list.service.ts
+++ src/app/firebase-services/note-list.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import { Firestore, collection} from '@angular/fire/firestore/firebase';
+import { inject } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NoteListService {
+  firestore: Firestore = inject(Firestore);
 
   constructor() { }
 }
